472b4f2c43a6bcc2ee3f0b36ffbac8a8706c3b49
andrewlo1111 <andrewlo2528@gmail.com>
2019-03-04 21:52:07 -0800

    basic private variables, functions, and slots

--- Documents/GradeCalculator/mainwindow.h
+++ Documents/GradeCalculator/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSlider>
+#include <QSpinBox>
+#include <QLabel>
+#include <vector>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +19,33 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void updateHW1(int);
+    void updateHW2(int);
+    void updateHW3(int);
+    void updateHW4(int);
+    void updateHW5(int);
+    void updateHW6(int);
+    void updateHW7(int);
+    void updateHW8(int);
+    void selectSchemaA();
+    void selectSchemaB();
+    void updateClass(int);
+
 private:
+    std::vector<int> hw_scores;
+    int midterm1;
+    int midterm2;
+    int final;
+    bool SchemaA;
+    QSlider* extra_slider;
+    QSpinBox* extra_spin;
+    QLabel* extra_label;
     Ui::MainWindow *ui;
+
+    //private functions
+    void updateScore();
+    unsigned int get_num_hw();
 };
 
 #endif // MAINWINDOW_H
